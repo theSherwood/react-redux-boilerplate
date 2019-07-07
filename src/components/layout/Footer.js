df11ab37deafc3c87fecaaef475c6a45caccc12e
admls <adam.the.sherwood@gmail.com>
2019-07-07 18:03:44 +0100

    init header and footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        position: "absolute",
+        background: "steelblue",
+        bottom: "0px",
+        left: "0px",
+        right: "0px"
+      }}
+    >
+      Footer
+      {/* Copyright &copy; 2019 Adam Sherwood */}
+    </footer>
+  );
+}
---
+++ src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header style={{ background: "steelblue" }}>
+      <div>
+        <h4 style={{ display: "inline-block" }}>Header</h4>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
